a4b05e2d117181fa65804e8bcae5aaa330d8cb41
Ruijie Yin <ruijieyin428@gmail.com>
2023-05-29 19:19:55 -0400

    added example data

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' Example data: gene expression profiles in liver cancer cell
+#'
+#' Dataset comes from Chen, Xin, et al. "Gene expression patterns in human
+#' liver cancers." Molecular biology of the cell 13.6 (2002): 1929-1939.
+#'
+#'
+#' @format
+#' A data frame with 143 rows and 86 columns:
+#' \describe{
+#'   \item{train_x_liver}{Training data x}
+#'   \item{train_y_liver}{Training data y}
+#'   \item{test_x_liver}{Test data x}
+#'   \item{test_y_liver}{Test data y}
+#'
+#' }
+#' @source {Data derived from Chen, Xin, et al. "Gene expression patterns in human
+#' liver cancers." Molecular biology of the cell 13.6 (2002): 1929-1939.}
+"liver_data"
